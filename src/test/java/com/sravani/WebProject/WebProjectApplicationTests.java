692c03dc0fb0ea9331785f030c61b49c5e6585f9
Sravani-Vunnam <sravaniii.vunnam@gmail.com>
2026-05-25 22:32:36 +0530

    adding springboot project created

---
+++ src/test/java/com/sravani/WebProject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sravani.WebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
